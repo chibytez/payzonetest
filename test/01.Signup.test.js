d14d066f25cb045090cc19ddcdd2e385ee8113ef
unknown <aniakuchibuike@gmail.com>
2021-05-08 11:49:39 +0100

    adding wallet

--- test/01.Signup.test.js
+++ test/01.Signup.test.js
@@ -7,20 +7,20 @@ const { expect } = require('chai');
 describe('End to End Test Suite', () => {
     const driver = new Builder().forBrowser('firefox').build();
 
-    it('should load page', () =>{
+    it('should launch', () =>{
         driver.get('https://uat.payzone.ng');
     })
 
-    it('it should sign out a user with inputs', async () =>{
+    it('it should sign up a user with inputs', async () =>{
         try {
            await driver.findElement(By.xpath(`//button[@title='Account']`)).click();
            await driver.findElement(By.xpath(`(//a[@class='pz-btn-group__btn'])[1]`)).click();
-           await driver.wait(until.elementLocated(By.xpath(`//input[@name='firstname']`)),2000).sendKeys("firtNameTest");
+           await driver.wait(until.elementLocated(By.xpath(`//input[@name='firstname']`)),2000).sendKeys("chibuike");
            await driver.wait(until.elementLocated(By.xpath(`//input[@name='lastname']`)),2000).sendKeys("lastNameTest");
-           await driver.findElement(By.xpath(`//input[@name='phone_number']`)).sendKeys("07060848190");
-           await driver.findElement(By.xpath(`//input[@name='email']`)).sendKeys("tester@gmail.com");
+           await driver.findElement(By.xpath(`//input[@name='phone_number']`)).sendKeys("08065751138");
+           await driver.findElement(By.xpath(`//input[@name='email']`)).sendKeys("chibuiky@gmail.com");
            await driver.findElement(By.xpath(`//input[@name='password']`)).sendKeys("password");
-           await driver.wait(until.elementLocated(By.xpath(`/html/body/div[3]/div[1]/div[2]/div/div/div[2]/div/div/div/div/div[1]/form/div/button`)),2000).click(); 
+           await driver.wait(until.elementLocated(By.xpath(`//button[@class='pz-btn pz-btn--lg position-relative pz-btn--primary pz-btn--wide']`)),2000).click(); 
         } catch (error) {
             console.log(error);
         }
---
+++ test/02.login.test.js
@@ -0,0 +1,40 @@
+const { Builder, By, Key, until, browser } = require('selenium-webdriver');
+const { expect } = require('chai');
+let millisecondsToWait = 5000;
+
+describe('Login and Setup Wallet', () => {
+    const driver = new Builder().forBrowser('firefox').build();
+
+    it('should launch', () =>{
+        driver.get('https://uat.payzone.ng');
+    })
+
+    it('it should Login a user', async () =>{
+        try {
+           await driver.findElement(By.xpath(`//button[@title='Account']`)).click();
+           await driver.findElement(By.xpath(`(//a[@class='pz-btn-group__btn'])[2]`)).click();
+           await driver.wait(until.elementLocated(By.xpath(`//input[@name='email']`)),2000).sendKeys("chibuiky@gmail.com");
+           await driver.findElement(By.xpath(`//input[@name='password']`)).sendKeys("password");
+           await driver.wait(until.elementLocated(By.xpath(`//button[@class='pz-btn pz-btn--lg position-relative pz-btn--primary pz-btn--wide']`)),2000).click();
+        } catch (error) {
+            console.log(error);
+        }   
+    })
+
+    it('it should create a wallet', async () => {
+        try {
+            await driver.wait(until.elementLocated(By.xpath(`//button[@class='wallet__value']`)),4000).click();
+            await driver.wait(until.elementLocated(By.xpath(`//input[@name='bvn']`)),4000).sendKeys("22163615608");
+            await driver.wait(until.elementLocated(By.xpath(`//input[@name='pin']`)),4000).sendKeys("1234");
+            await driver.wait(until.elementLocated(By.xpath(`//input[@name='pin_confirmation']`)),4000).sendKeys("1234");
+            setTimeout(function() {
+               driver.wait(until.elementLocated(By.xpath(`//button[@type='submit']`)),4000).click();
+            }, millisecondsToWait);
+            
+        }catch (error) {
+        console.log(error);
+        }
+    })
+ 
+})
+     
