deb489087b755fc47ab195666c4b4b0f84a63920
unknown <aniakuchibuike@gmail.com>
2021-05-09 10:34:33 +0100

    adding bvn

--- test/02.login.test.js
+++ test/02.login.test.js
@@ -1,6 +1,10 @@
 const { Builder, By, Key, until, browser } = require('selenium-webdriver');
 const { expect } = require('chai');
-let millisecondsToWait = 5000;
+// let millisecondsToWait = 5000;
+
+// setTimeout(function() {
+//     // Whatever you want to do after the wait
+// }, millisecondsToWait);
 
 describe('Login and Setup Wallet', () => {
     const driver = new Builder().forBrowser('firefox').build();
@@ -14,7 +18,8 @@ describe('Login and Setup Wallet', () => {
            await driver.findElement(By.xpath(`//button[@title='Account']`)).click();
            await driver.findElement(By.xpath(`(//a[@class='pz-btn-group__btn'])[2]`)).click();
            await driver.wait(until.elementLocated(By.xpath(`//input[@name='email']`)),2000).sendKeys("chibuiky@gmail.com");
-           await driver.findElement(By.xpath(`//input[@name='password']`)).sendKeys("password");
+           await driver.findElement(By.xpath(`//input[@ 
+           name='password']`)).sendKeys("password");
            await driver.wait(until.elementLocated(By.xpath(`//button[@class='pz-btn pz-btn--lg position-relative pz-btn--primary pz-btn--wide']`)),2000).click();
         } catch (error) {
             console.log(error);
@@ -27,12 +32,10 @@ describe('Login and Setup Wallet', () => {
             await driver.wait(until.elementLocated(By.xpath(`//input[@name='bvn']`)),4000).sendKeys("22163615608");
             await driver.wait(until.elementLocated(By.xpath(`//input[@name='pin']`)),4000).sendKeys("1234");
             await driver.wait(until.elementLocated(By.xpath(`//input[@name='pin_confirmation']`)),4000).sendKeys("1234");
-            setTimeout(function() {
-               driver.wait(until.elementLocated(By.xpath(`//button[@type='submit']`)),4000).click();
-            }, millisecondsToWait);
-            
-        }catch (error) {
-        console.log(error);
+            await driver.wait(until.elementLocated(By.xpath(`//i[@class='fa fa-check text-success']`)),4000).click();
+            await driver.wait(until.elementLocated(By.xpath(`//button[@type='submit']`)),4000).click();
+        } catch (error) {
+            console.log(error);
         }
     })
  
